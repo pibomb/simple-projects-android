1d7714f499771769708c38ac94a6938682a41726
pibomb <pibomb988@gmail.com>
2016-08-11 19:10:03 -0400

    SimpleMathQuiz: Created Math Question
    A MathQuestion class that randomly creates a math question with
    addition, subtraction, multiplication, division, or modulus.

---
+++ SimpleMathQuiz/app/src/main/java/com/eagleblitz/morris/simplemathquiz/MathQuestion.java
@@ -0,0 +1,81 @@
+package com.eagleblitz.morris.simplemathquiz;
+
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ * Created by mcmor on 2016-08-11.
+ */
+public class MathQuestion {
+
+    public static final int ADDITION = 0;
+    public static final int SUBTRACTION = 1;
+    public static final int MULTIPLICATION = 2;
+    public static final int DIVISION = 3;
+    public static final int MODULUS = 4;
+
+    Random random;
+
+    int num1, num2;
+    int operation;
+
+    public MathQuestion() {
+        random = new Random();
+
+        operation = random.nextInt(5);
+
+        switch(operation) {
+            case ADDITION:
+                num1 = random.nextInt(100);
+                num2 = random.nextInt(100-num1);
+                break;
+            case SUBTRACTION:
+                num1 = random.nextInt(100);
+                num2 = random.nextInt(num1);
+                break;
+            case MULTIPLICATION:
+                num1 = random.nextInt(13);
+                num2 = random.nextInt(13);
+                break;
+            case DIVISION:
+                num2 = random.nextInt(13);
+                num1 = random.nextInt(13) * num2;
+                break;
+            case MODULUS:
+                num2 = random.nextInt(13);
+                num1 = random.nextInt(100-num2) + num2;
+                break;
+            default:
+                num1 = 0;
+                num2 = 0;
+        }
+    }
+
+    public int answer() {
+        switch (operation) {
+            case ADDITION: return num1 + num2;
+            case SUBTRACTION: return num1 - num2;
+            case MULTIPLICATION: return num1 * num2;
+            case DIVISION: return num1 / num2;
+            case MODULUS: return num1 % num2;
+            default: return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "%d %c %d = ";
+
+        char operator;
+        switch (operation) {
+            case ADDITION: operator = '+'; break;
+            case SUBTRACTION: operator = '-'; break;
+            case MULTIPLICATION: operator = '*'; break;
+            case DIVISION: operator = '/'; break;
+            case MODULUS: operator = '%'; break;
+            default: operator = '?';
+        }
+
+        return String.format(Locale.getDefault(), result, num1, operator, num2);
+    }
+}
